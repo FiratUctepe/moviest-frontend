dcb044f98fa1fa5a0a280d8a6d6341167f364b17
FiratUctepe <firat.uctepe1@gmail.com>
2023-02-28 22:56:12 +0300

    ReviewForm nad Reviews created

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
-import {Routes,Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
-
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
-
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState({});
+  const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () => {
+  const getMovies = async () =>{
+    
+    try
+    {
 
-    try {
-      const response = await axios.get('http://localhost:8080/api/v1/movies');
+      const response = await axios.get("/api/v1/movies");
+
       setMovies(response.data);
-    } catch (error) {
-      console.log(error);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
     }
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
     getMovies();
   },[])
 
-
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home movies={movies}/>}/>
-          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}/>
-        </Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
       </Routes>
+
     </div>
   );
 }
